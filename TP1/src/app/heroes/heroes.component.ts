9d63783f7071898e40568c843498af53b35b1ef7
Patrick de-SOUZA <emiliodesouza800@gmail.com>
2020-12-03 15:34:40 +0100

    TP6 Services Final

--- TP1/src/app/heroes/heroes.component.ts
+++ TP1/src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit} from '@angular/core';
-import { Hero } from 'src/app/hero';
-import { HEROES } from '../mock-heroes';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 
 @Component({
@@ -9,16 +10,21 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  constructor() { }
-  ngOnInit(): void {
+
+  selectedHero: Hero;
+  heroes: Hero[];
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
+
+  ngOnInit() {
+    this.getHeroes();
   }
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  }; 
-  heroes = HEROES;
-  selectedHero: Hero
+  
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
 }
